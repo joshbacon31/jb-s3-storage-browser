b6996907ff209a1e2bbf1f79df1dcc4b5d2ce022
joshbacon31 <josh.bacon@agorocarbon.com>
2025-07-14 13:38:28 -0400

    Added storage backend

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -7,7 +7,7 @@ export const storage = defineStorage({
       allow.guest.to(['read']),
       allow.authenticated.to(['read', 'write', 'delete']),
     ],
-    'protected/{entity_id}/*': [
+    'protectedInternal/{entity_id}/*': [
       allow.authenticated.to(['read']),
       allow.entity('identity').to(['read', 'write', 'delete'])
     ],
